965828ed73c61d321cce9a4911a80e6d7968e229
jzheng0003 <jzheng@bowdoin.edu>
2023-05-01 19:15:09 -0400

    [Implementation] Login functionality

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 
-var mentorRadioButton;
-var menteeRadioButton;
-
 var users = JSON.parse(localStorage.getItem('Users')) || [];
 
 function mentorClick() {
     alert("Your mentor has been notified!");
 }
 
-function navigateToPage() {
-
-    if (mentorRadioButton.checked) {
-      window.location.href = './mentorview.html';
-    } else if (menteeRadioButton.checked) {
-      window.location.href = './Studentview/index.html';
+function navigateToPage(userType) {
+    switch (userType) {
+        case "Mentor":
+            window.location.href = './mentorview.html';
+            break;
+        case "Mentee":
+            window.location.href = './Studentview/index.html';
+            break;
     }
 }
 
@@ -21,10 +20,15 @@ function isEmpty(string) {
     return !string.trim().length;
 }
 
+/**
+ * Registers the use based on the four fields.
+ * Validates that the fields are not empty and passwords match.
+ * Method stores user data in local storage.
+ */
 function registration() {
-    var users = JSON.parse(localStorage.getItem('Users')) || [];
-    mentorRadioButton = document.getElementById('mentor');
-    menteeRadioButton = document.getElementById('mentee');    
+    users = JSON.parse(localStorage.getItem('Users')) || [];
+    var mentorRadioButton = document.getElementById('mentor');
+    var menteeRadioButton = document.getElementById('mentee');    
     var userData;
     var userType;
 
@@ -42,10 +46,10 @@ function registration() {
                 userType = "Mentee";
             }
     
-            userData = [{Name:document.getElementById("name").value},
-                {Email:document.getElementById("email").value},
-                {Password:document.getElementById("psw").value},
-                {UserType:userType}];
+            userData = {Name: document.getElementById("name").value,
+                Email: document.getElementById("email").value,
+                Password: document.getElementById("psw").value,
+                UserType :userType};
     
             users.push(userData);
             localStorage.setItem('Users', JSON.stringify(users));
@@ -62,7 +66,27 @@ function registration() {
         alert("Registration fields not complete.");
     }
 
+}
 
-    
+/**
+ * Checks the validity of user's login email and password
+ */
+function checkLogin() {
+    users = JSON.parse(localStorage.getItem('Users'));
 
+    let emailInput = document.getElementById("login").value;
+    let passInput = document.getElementById("password").value;
+    let userType;
+
+    const fieldsMatch = this.users.find( (user) => {
+        userType = user.UserType;
+        return user.Email === emailInput && user.Password === passInput;
+        });
+
+    if (fieldsMatch) {
+        navigateToPage(userType);
+    }
+    else {
+        alert("Incorrect email or password.");
+    }
 }
