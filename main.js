515ff868d755948a74f090bad00b5b13fe149501
jzheng0003 <jzheng@bowdoin.edu>
2023-05-15 22:31:30 -0400

    Removed some fonts

--- main.js
+++ main.js
@@ -9,10 +9,6 @@ function mentorClick() {
     alert("Your mentor has been notified!");
 }
 
-function displayReachedOut() {
-    alert("Your mentor has been pooped!")
-}
-
 function navigateToPage(userType) {
     switch (userType) {
         case "Mentor":
